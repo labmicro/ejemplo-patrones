5e15d18352ab55321e972a6a69daa4cf8d2379af
Esteban Volentini <evolentini@herrera.unt.edu.ar>
2025-05-22 18:15:15 -0300

    Ejemplo de observador

---
+++ observer/src/keyboard.c
@@ -0,0 +1,165 @@
+/*********************************************************************************************************************
+Copyright 2016-2025, Laboratorio de Microprocesadores
+Facultad de Ciencias Exactas y Tecnología
+Universidad Nacional de Tucuman
+http://www.microprocesadores.unt.edu.ar/
+Copyright 2016-2025, Esteban Volentini <evolentini@herrera.unt.edu.ar>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*********************************************************************************************************************/
+
+/** @file keyboard.c
+ ** @brief Implementación de la clase para el manejo del teclado
+ **/
+
+/* === Headers files inclusions ==================================================================================== */
+
+#include "keyboard.h"
+#include <stdio.h>
+#include <pthread.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/* === Macros definitions ========================================================================================== */
+
+/* === Private data type declarations ============================================================================== */
+
+/** Puntero a una estructura cliente */
+typedef struct client_s * client_t;
+
+/** Estructura para un cliente suscritos al teclado */
+struct client_s {
+    char from;
+    char to;
+    keyboard_event_t EventHandler;
+    client_t previous;
+    client_t next;
+};
+
+/** Estructura del objeto teclado */
+struct keyboard_s {
+    client_t clients;
+};
+
+/* === Private function declarations =============================================================================== */
+
+/**
+ * @brief Configura el terminal para la lectura de teclas
+ */
+static void KeyboardConfigure(void);
+
+/**
+ * @brief Busca un cliente por su manejador de eventos
+ *
+ * @param self    Puntero al teclado
+ * @param handler Manejador de eventos a buscar
+ * @return Puntero al cliente encontrado o NULL si no existe
+ */
+static client_t KeyboardFind(keyboard_t self, keyboard_event_t handler);
+
+/* === Private variable definitions ================================================================================ */
+
+/* === Public variable definitions ================================================================================= */
+
+/* === Private function definitions ================================================================================ */
+
+static void KeyboardConfigure(void) {
+    struct termios ttystate;
+
+    tcgetattr(STDIN_FILENO, &ttystate);
+    ttystate.c_lflag &= (~ICANON & ~ECHO);
+    ttystate.c_cc[VMIN] = 1;
+    tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
+}
+
+static client_t KeyboardFind(keyboard_t self, keyboard_event_t handler) {
+    client_t client = self->clients;
+    while (client != NULL) {
+        if (client->EventHandler == handler) {
+            return client;
+        }
+        client = client->next;
+    }
+    return NULL;
+}
+
+/* === Public function implementation ============================================================================== */
+
+keyboard_t KeyboardCreate(void) {
+    keyboard_t self = malloc(sizeof(struct keyboard_s));
+    if (self) {
+        KeyboardConfigure();
+        self->clients = NULL;
+    }
+    return self;
+}
+
+bool KeyboardSubscribe(keyboard_t self, char from, char to, keyboard_event_t handler) {
+    client_t client = NULL;
+
+    if ((self) && (handler) && (from < to)) {
+        client = KeyboardFind(self, handler);
+        if (client == NULL) {
+            client = malloc(sizeof(struct client_s));
+            client->EventHandler = handler;
+            client->next = self->clients;
+            if (client->next) {
+                client->next->previous = client;
+            }
+            client->previous = NULL;
+            self->clients = client;
+        }
+        client->from = from;
+        client->to = to;
+    }
+    return (client != NULL);
+}
+
+bool KeyboardRemove(keyboard_t self, keyboard_event_t handler) {
+    client_t client = NULL;
+
+    if (self) {
+        client = KeyboardFind(self, handler);
+        if (client) {
+            if (client->previous) {
+                client->previous->next = client->next;
+            } else {
+                self->clients = client->next;
+            }
+            if (client->next) {
+                client->next->previous = client->previous;
+            }
+            free(client);
+        }
+    }
+    return (client != NULL);
+}
+
+void KeyboardTick(keyboard_t self) {
+    char key = (char)getchar();
+    if (key) {
+        client_t client = self->clients;
+        while (client != NULL) {
+            if ((client->from <= key) && (key <= client->to)) {
+                client->EventHandler(self, key);
+            }
+            client = client->next;
+        }
+    }
+}
+
+/* === End of documentation ======================================================================================== */
